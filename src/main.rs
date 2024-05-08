e47e1faa09e399ab67d8f188311007e87950a2a1
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2024-05-08 13:18:18 +0200

    Word counter and common word finder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+mod text_analysis;
+
+use std::env;
+use std::fs;
+use text_analysis::count_words;
+use text_analysis::common_word_finder;
+
 fn main() {
-    println!("Hello, world!");
+
+    // File path to the text file
+    let file_path = "test.txt";
+
+    // Read the contents of the file
+    let contents = match fs::read_to_string(file_path) {
+        Ok(contents) => contents,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            return;
+        }
+    };
+
+    let count_result = count_words(contents.clone());
+
+    let count = match count_result {
+        Ok(count) => count,
+        Err(err) => {
+            eprintln!("Error counting words: {}", err);
+            return;
+        }
+    };
+
+    let common_words_result = common_word_finder(contents.clone());
+
+    println!("Contents: \n{contents}");
+    println!("Count: {count}");
+    println!("Common Words: {:?}", common_words_result);
 }
